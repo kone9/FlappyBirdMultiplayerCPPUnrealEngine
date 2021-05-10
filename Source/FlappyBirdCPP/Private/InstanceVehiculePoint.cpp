e880dea10df57d2c444b83db5dc8ccda9b801e08
Ariel Gimenez <arielgimenez_95@hotmail.com>
2021-05-10 13:58:46 -0300

    instanciando con un blueprint

---
+++ Source/FlappyBirdCPP/Private/InstanceVehiculePoint.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InstanceVehiculePoint.h"
+
+// Sets default values
+AInstanceVehiculePoint::AInstanceVehiculePoint()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AInstanceVehiculePoint::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AInstanceVehiculePoint::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/FlappyBirdCPP/Public/InstanceVehiculePoint.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "InstanceVehiculePoint.generated.h"
+
+UCLASS()
+class FLAPPYBIRDCPP_API AInstanceVehiculePoint : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AInstanceVehiculePoint();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	UPROPERTY(EditAnywhere)
+	TArray<AActor*> vehicules;
+};
