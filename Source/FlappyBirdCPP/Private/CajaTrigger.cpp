896c392be959ccc290bdf34130f4d470274984a9
Ariel Gimenez <arielgimenez_95@hotmail.com>
2021-05-11 18:06:34 -0300

    los vehiculos se destruyen pero el rendimiento es muy malo

--- Source/FlappyBirdCPP/Private/CajaTrigger.cpp
+++ Source/FlappyBirdCPP/Private/CajaTrigger.cpp
@@ -2,4 +2,36 @@
 
 
 #include "CajaTrigger.h"
+#include "DrawDebugHelpers.h"
 
+
+//Metodo contructor
+ACajaTrigger::ACajaTrigger()
+{
+}
+
+//metodo beginPlay
+void ACajaTrigger::BeginPlay()
+{
+	Super::BeginPlay();
+	if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("inicio el juego!"));
+	
+	DrawDebugBox(GetWorld(), GetActorLocation(), GetComponentsBoundingBox().GetExtent(), FColor::Purple, true, -1, 0, 100);
+	
+	OnActorBeginOverlap.AddDynamic(this, &ACajaTrigger::OnOverlapBegin);//siempre declarar la dinamica del overlapBegin, coloco en metodo contructor porque es de este tipo, sino ojo conviene en beginPlay
+
+}
+
+void ACajaTrigger::OnOverlapBegin(class AActor* OverlappedActor, class AActor* OtherActor)
+{
+	if (OtherActor != nullptr)
+	{
+		OtherActor->Destroy();
+	}
+	if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("entro un cuerpo al area!"));
+	
+	/*if (OtherActor && (OtherActor != this))
+	{
+
+	}*/
+}
--- Source/FlappyBirdCPP/Public/CajaTrigger.h
+++ Source/FlappyBirdCPP/Public/CajaTrigger.h
@@ -13,5 +13,15 @@ UCLASS()
 class FLAPPYBIRDCPP_API ACajaTrigger : public ATriggerBox
 {
 	GENERATED_BODY()
+
+public:
+	ACajaTrigger();//metodo contructor
+
+	UFUNCTION()
+		void OnOverlapBegin(class AActor* OverlappedActor, class AActor* OtherActor);
+
+protected:
+	virtual void BeginPlay() override;
+
 	
 };
