d923e71480e8be6ad0d82a60f3f4ae1781420e19
Ariel Gimenez <arielgimenez_95@hotmail.com>
2021-05-10 23:02:54 -0300

    agragada clase triggerBox pero aun no funciona

---
+++ Source/FlappyBirdCPP/Public/CajaTrigger.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TriggerBox.h"
+#include "CajaTrigger.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLAPPYBIRDCPP_API ACajaTrigger : public ATriggerBox
+{
+	GENERATED_BODY()
+	
+};
