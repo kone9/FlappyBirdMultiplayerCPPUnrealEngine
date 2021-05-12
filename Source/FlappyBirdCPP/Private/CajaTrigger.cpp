f8b7fd674ffbd24dc5b5f75f1caf4a91ad728aab
Ariel Gimenez <arielgimenez_95@hotmail.com>
2021-05-12 20:51:56 -0300

    agregando neblina y sacando bordes de la colision para reiniciar

--- Source/FlappyBirdCPP/Private/CajaTrigger.cpp
+++ Source/FlappyBirdCPP/Private/CajaTrigger.cpp
@@ -2,7 +2,7 @@
 
 
 #include "CajaTrigger.h"
-#include "DrawDebugHelpers.h"
+//#include "DrawDebugHelpers.h"
 
 
 //Metodo contructor
@@ -16,7 +16,7 @@ void ACajaTrigger::BeginPlay()
 	Super::BeginPlay();
 	if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("inicio el juego!"));
 	
-	DrawDebugBox(GetWorld(), GetActorLocation(), GetComponentsBoundingBox().GetExtent(), FColor::Purple, true, -1, 0, 100);
+	//DrawDebugBox(GetWorld(), GetActorLocation(), GetComponentsBoundingBox().GetExtent(), FColor::Purple, true, -1, 0, 100);
 	
 	OnActorBeginOverlap.AddDynamic(this, &ACajaTrigger::OnOverlapBegin);//siempre declarar la dinamica del overlapBegin, coloco en metodo contructor porque es de este tipo, sino ojo conviene en beginPlay
 
