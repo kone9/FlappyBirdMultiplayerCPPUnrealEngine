e407a373d8a0abd45a1feee07f8babd4243e81cb
Ariel Gimenez <arielgimenez_95@hotmail.com>
2021-05-14 19:37:25 -0300

    funciona las etiquetas correctamente

--- Source/FlappyBirdCPP/Private/BoxTriggerDestroy.cpp
+++ Source/FlappyBirdCPP/Private/BoxTriggerDestroy.cpp
@@ -3,9 +3,14 @@
 
 #include "BoxTriggerDestroy.h"
 
-void ACajaTrigger::OnOverlapBegin(class AActor* OverlappedActor, class AActor* OtherActor)
+void ABoxTriggerDestroy::BeginPlay()
 {
-	
+	OnActorBeginOverlap.AddDynamic(this, &ABoxTriggerDestroy::OnOverlapBegin);
+}
+
+void ABoxTriggerDestroy::OnOverlapBegin(class AActor* OverlappedActor, class AActor* OtherActor)
+{
+	if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Entro un vehiculo y tendría que destruirse!"));
 	if (OtherActor != nullptr)//como son punteros siempre verificar que no sea nulo
 	{
 		if (OtherActor->ActorHasTag("obstacleVehicule"))//si tiene la etiqueta obstacleVehicule
