f0b37f0958e7c537e9daa1faa4692ee0ef110a39
Ariel Gimenez <arielgimenez_95@hotmail.com>
2021-05-15 16:25:17 -0300

    insntanciando correctamente en diferentes posiciones con blueprint y C++

--- Source/FlappyBirdCPP/Private/BoxTriggerDestroy.cpp
+++ Source/FlappyBirdCPP/Private/BoxTriggerDestroy.cpp
@@ -10,7 +10,7 @@ void ABoxTriggerDestroy::BeginPlay()
 
 void ABoxTriggerDestroy::OnOverlapBegin(class AActor* OverlappedActor, class AActor* OtherActor)
 {
-	if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Entro un vehiculo y tendría que destruirse!"));
+	//if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Entro un vehiculo y tendría que destruirse!"));
 	if (OtherActor != nullptr)//como son punteros siempre verificar que no sea nulo
 	{
 		if (OtherActor->ActorHasTag("obstacleVehicule"))//si tiene la etiqueta obstacleVehicule
